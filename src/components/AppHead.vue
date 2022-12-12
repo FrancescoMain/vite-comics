cfa89ef371fef3566c229e688bf50a5efe3e9023
Francesco <cesaranofrancescomain@gmail.com>
2022-12-12 15:44:09 +0100

    aggiunta struttura navbar con stile

--- src/components/AppHead.vue
+++ src/components/AppHead.vue
@@ -5,64 +5,115 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="logo">
-            <img src="../../public/img/dc-logo.png" alt="">
+
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../../public/img/dc-logo.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                    <li>
+                        <a href="">Link</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-        <ul>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-            <li>
-                <a href="">Link</a>
-            </li>
-        </ul>
-    </div>
+    </header>
+
 </template>
 
 <style lang="scss" scoped>
-@use '../styles/general.scss' as *;
 @use '../styles/partials/mixins' as *;
+@use '../styles/partials/variable' as *;
 
-.container {
-    @include center (space);
-    height: 100px;
-    background-color: aqua;
+header {
+    height: 120px;
+    @include center ();
 
-    ul {
-        list-style: none;
-        @include center ();
+    .container {
+        @include center (space);
+        width: 66%;
+        height: 100%;
 
-        li {
+        .logo {
 
-            a {
-                text-decoration: none;
+            @include center ();
+            height: 100%;
+
+            img {
+                width: 80px;
             }
         }
+
+        nav {
+
+            width: 58%;
+            height: 100%;
+
+            ul {
+                list-style: none;
+                @include center ();
+                width: 100%;
+                height: 100%;
+
+                li {
+                    width: calc(100% / 10);
+                    height: 100%;
+                    @include center ();
+
+
+                    a {
+                        text-decoration: none;
+                    }
+
+                    &:active,
+                    &:hover {
+                        border-bottom: $primary 4px solid;
+
+                        a {
+                            color: $primary;
+
+                        }
+
+                    }
+
+
+                }
+
+
+
+            }
+
+        }
+
 
     }
 }
