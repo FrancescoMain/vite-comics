ee6bb4784f5e4cde16de7c31bd5f82f9c0150ae2
Francesco <cesaranofrancescomain@gmail.com>
2022-12-13 15:20:31 +0100

    creato componente button

---
+++ src/components/JumboButton.vue
@@ -0,0 +1,45 @@
+<script >
+export default {
+    methods: {
+        getImageUrl: function (imgPath) {
+            return new URL(`../../public/img/${imgPath}`, import.meta.url).href;
+        }
+    }
+
+};
+</script>
+
+<template>
+
+    <button class="button">CURRENT SERIES</button>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variable' as *;
+
+
+
+.button {
+    width: 180px;
+    height: 45px;
+    font-size: 0.9rem;
+    text-transform: uppercase;
+    font-weight: 700;
+    color: $secondary;
+    background-color: $primary;
+    border: none;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    cursor: pointer;
+    outline: none;
+
+    &:hover {
+        background-color: $secondary;
+        box-shadow: 0px 15px 20px $trd;
+        color: rgb(0, 0, 0);
+        transform: translateY(-7px);
+    }
+}
+</style>
