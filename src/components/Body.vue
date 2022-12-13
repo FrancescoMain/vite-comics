e56a2d16a386ae32f26c1c11753a64779680f856
Francesco <cesaranofrancescomain@gmail.com>
2022-12-13 14:59:56 +0100

    Creato componente body con componente jumbo

---
+++ src/components/Body.vue
@@ -0,0 +1,33 @@
+<script >
+import Jumbo from './Jumbo.vue'
+
+export default {
+    components: {
+        Jumbo
+    },
+    data() {
+        return {
+
+
+        };
+
+    },
+    methods: {
+        getImageUrl: function (imgPath) {
+            return new URL('../../public/img/${imgPath}');
+        }
+    }
+
+};
+</script>
+
+<template>
+
+    <Jumbo />
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variable' as *;
+</style>
