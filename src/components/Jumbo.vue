4361cf7de7cc559474e13ff1940c989afc644cfa
Francesco <cesaranofrancescomain@gmail.com>
2022-12-13 15:00:05 +0100

    Creato componente jumbo

---
+++ src/components/Jumbo.vue
@@ -0,0 +1,25 @@
+<script >
+export default {
+    methods: {
+        getImageUrl: function (imgPath) {
+            return new URL(`../../public/img/${imgPath}`, import.meta.url).href;
+        }
+    }
+
+};
+</script>
+
+<template>
+    <div class="jumbo"></div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variable' as *;
+
+.jumbo {
+    background-image: url("../../public/img/jumbotron.jpg");
+    height: 400px;
+    width: 100%;
+}
+</style>
